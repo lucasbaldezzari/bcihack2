a7e6ec4757ab700229b67366343b6108086d14ab
lucasbaldezzari <lmbaldezzari@gmail.com>
2023-03-01 16:36:40 -0300

    Se edita métodos de FilterModule para transformarlo en una clase con métodos del tipo fit() y transform() para usarlo dentro de un pipline durante las fases de entrenamiento y online

--- Desarrollo/PythonScripts/SignalProcessor/FilterModule.py
+++ Desarrollo/PythonScripts/SignalProcessor/FilterModule.py
@@ -1,24 +1,55 @@
 import numpy as np
 from scipy import signal
 from scipy.signal import butter, lfilter, freqz, filtfilt, iirnotch, iirfilter
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_array, check_is_fitted
 
-class SignalProcessor():
+class SignalProcessor(BaseEstimator, TransformerMixin):
     """Clase para procesar señales provenientes del generador de señales (openBCI boards). Las señales que entran son un numpy array en la forma
     [canales, muestras]. La idea es aplicar un filtro pasa banda y un filtro notch a la señal a todo el array."""
-    def __init__(self, sample_rate, lowcut, highcut, notch_freq, notch_width):
+    def __init__(self, sample_rate):
         """Inicializa el objeto con los parámetros de filtrado."""
         self.sample_rate = sample_rate
+
+    def fit(self, X = None, y=None, lowcut = 1.0, highcut = 36.0, notch_freq = 50.0, notch_width = 2.0):
+        """Creamos los filtros.
+        -lowcut: Frecuencia de corte inferior del filtro pasa banda.
+        -highcut: Frecuencia de corte superior del filtro pasa banda.
+        -notch_freq: Frecuencia de corte del filtro notch.
+        -notch_width: Ancho de banda del filtro notch.
+        -X: Señal de entrada. No se usa en este caso.
+        -y: No se usa en este caso."""""
+
         self.lowcut = lowcut
         self.highcut = highcut
         self.notch_freq = notch_freq
         self.notch_width = notch_width
 
-        self.b, self.a = self.butter_bandpass(lowcut, highcut, sample_rate, order=5)
-        self.b_notch, self.a_notch = self.iirnotch(notch_freq, notch_width, sample_rate)
+        self.b, self.a = butter(5, [self.lowcut, self.highcut], btype='bandpass', fs=self.sample_rate)
+        self.b_notch, self.a_notch = iirnotch(self.notch_freq, self.notch_width, self.sample_rate)
 
-    def filterSignal(self, signal):
+        return self #el método fit siempre debe retornar self!
+
+    def transform(self, signal):
         """Función para aplicar los filtros a la señal.
-        -signal: numpy array de la forma [canales, muestras]."""
-        signalFiltered = filtfilt(self.b, self.a, signal, axis=1)
-        signalFiltered = filtfilt(self.b_notch, self.a_notch, signal, axis=1)
-        return signalFiltered
+        -signal: Es la señal en un numpy array de la forma [canales, muestras]."""
+
+        signal = filtfilt(self.b, self.a, signal, axis=1)
+        signal = filtfilt(self.b_notch, self.a_notch, signal, axis=1)
+        return signal
+
+def main():
+
+    with open('testsignal.npy', 'rb') as f:
+        signal = np.load(f)
+
+    # signal.shape
+
+    signalpros = SignalProcessor(sample_rate=250)
+    signalpros.fit(lowcut=1.0, highcut=36.0, notch_freq=50.0, notch_width=2.0)
+    signalFiltered = signalpros.transform(signal)
+    # signalFiltered = signalpros.fit_transform(signal,highcut=36.0, notch_freq=50.0, notch_width=2.0)
+
+
+if __name__ == "__main__":
+    main()
