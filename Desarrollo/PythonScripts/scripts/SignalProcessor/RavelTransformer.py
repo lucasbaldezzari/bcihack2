beff3b1a7e0542b71f12637628f11d3c2b993dad
lucasbaldezzari <lmbaldezzari@gmail.com>
2023-03-08 18:18:33 -0300

    Se corrigen comentarios

--- Desarrollo/PythonScripts/scripts/SignalProcessor/RavelTransformer.py
+++ Desarrollo/PythonScripts/scripts/SignalProcessor/RavelTransformer.py
@@ -1,8 +1,6 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 import numpy as np
 
-#This class recieve data in the form [n_trials, n_channels or n_components, n_samples] and return data in the form [n_trials, n_components x n_samples]
-#the idea is make a ravel of the data in the second dimension and return a matrix with the shape [n_trials, n_components x n_samples] in order to fit a classifier
 class RavelTransformer(BaseEstimator, TransformerMixin):
     """Esta clase recibe datos en la forma [n_trials, n_channels or n_components, n_samples] y retorna datos en la forma [n_trials, n_components x n_samples]"""
     def __init__(self, method = "reshape"):
