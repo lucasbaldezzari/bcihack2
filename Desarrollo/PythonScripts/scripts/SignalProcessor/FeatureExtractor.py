369eaaafb48e774f77dc8267f403a8e9825236e3
lucasbaldezzari <lmbaldezzari@gmail.com>
2023-03-08 10:19:34 -0300

    Se actualiza FeatureExtractor

--- Desarrollo/PythonScripts/scripts/SignalProcessor/FeatureExtractor.py
+++ Desarrollo/PythonScripts/scripts/SignalProcessor/FeatureExtractor.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.signal import hilbert
+from scipy.signal import hilbert, welch
 from sklearn.base import BaseEstimator, TransformerMixin
 from matplotlib import mlab
 
@@ -13,14 +13,14 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
     Por ejemplo, se puede implementar ICA para eliminar las componentes de ruido de la señal y luego aplicar la transformada de Hilbertm, o bien
     implementar CSP para extraer las componentes de interés de la señal y luego aplicar la transformada de Hilbert."""
 
-    def __init__(self, method = "hilbert", sample_rate = 250., band = None):
+    def __init__(self, method = "welch", sample_rate = 250., overlap = 0.5):
         """No se inicializan atributos.
         - method: método por el cual extraer la potencia
-        - band: lista con el ancho de banda de frecuencias a extraer. Ejemplo [8,30]. Si es None se retorna array completo"""
+        - overlap: solapamiento en segundos"""
 
         self.method = method
         self.sample_rate = sample_rate
-        self.band = band
+        self.overlap = overlap
 
     def fit(self, X = None, y=None):
         """No hace nada"""
@@ -29,43 +29,28 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
 
     def transform(self, signal):
         """Función para aplicar los filtros a la señal.
-        -signal: Es la señal en un arreglo de numpy de la forma [canales, muestras].
+        -signal: Es la señal en un arreglo de numpy de la forma [canales, muestras, n_trials].
         
-        Retorna: Un arreglo de numpy con las características de la señal. La forma del arreglo es [canales, power_sample, trials]"""
-
-        #NOTA: Debemos evaluar si implementamos un CSP para seleccionar los mejores canales y luego aplicamos la transformada de Hilbert
-
-        if self.method == "hilbert":
-            """Retorna la potencia de la señal en la forma [canales, power_sample, trials]"""
+        Retorna: Un arreglo de numpy con las características de la señal. La forma del arreglo es [canales, power_sample, n_trials]"""
+        
+        if self.method == "welch":
+            """Retorna la potencia de la señal en la forma [canales, power_sample, n_trials]"""
             #Aplicamos la transformada de Hilbert
-            transformedSignal = hilbert(signal, axis=1) #trnasformada de Hilbert
-            power = np.abs(transformedSignal)**2 #Calculamos la potencia de la señal
-            self._alphaPower = power[:, 8:13]#.mean(axis=1) #Potencia media en la banda alfa
-            self._betaPower = power[:, 13:30]#.mean(axis=1) #Potencia media en la banda beta
-            features = np.hstack((self._alphaPower, self._betaPower)) #apilamos las características
+            self.freqs, self.power = welch(signal, axis=1, nfft = signal.shape[1],
+                                           noverlap=int(self.sample_rate*self.overlap)) #trnasformada de Welch
+            self.freqs = self.freqs*self.sample_rate
 
-            features = power
-
-            return features
+            return self.power
         
-        elif self.method == "psd":
+        if self.method == "psd":
             #Calcula la PSD de la señal
-            if self.band:
-                psdfunc = lambda x: mlab.psd(x, NFFT = signal.shape[1], Fs = self.sample_rate)
-                psd = np.apply_along_axis(psdfunc, 1, signal)
-                self.freqs = psd[0,1,:]
-                f1 = (self.freqs>=self.band[0])
-                f2 = (self.freqs<=self.band[1])
-                self.freqs = self.freqs[f1 & f2]
-                self.power = psd[:,0,:]
-                self.power = self.power[:,f1 & f2]
-                return self.power
-            else:
-                psdfunc = lambda x: mlab.psd(x, NFFT = signal.shape[1], Fs = self.sample_rate)
-                psd = np.apply_along_axis(psdfunc, 1, signal)
-                self.power = psd[:,0,:]
-                self.freqs = psd[0,1,:]
-                return self.power
+            psdfunc = lambda x: mlab.psd(x, NFFT = signal.shape[1], Fs = self.sample_rate,
+                                         noverlap=int(self.sample_rate*self.overlap))
+            psd = np.apply_along_axis(psdfunc, 1, signal)
+            self.freqs = psd[0,1,:,0]
+            self.power = psd[:,0,:]
+
+            return self.power
 
     def fit_transform(self, signal):
         """Función para aplicar los filtros a la señal.
@@ -75,28 +60,53 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
         return self.transform(signal)
 
 if __name__ == '__main__':
-    with open("testsignal_filtered.npy", "rb") as f:
-        signal = np.load(f)
-    
-    featureExtractor = FeatureExtractor(method="psd", sample_rate=250, band = None) #instanciamos el extractor de características
 
-    features = featureExtractor.fit_transform(signal) #signal [n_channels, n_samples]
-    features.shape
+    with open("all_left_trials.npy", "rb") as f:
+        signalLeft = np.load(f)
 
-    featureExtractor.freqs.shape
+    with open("all_right_trials.npy", "rb") as f:
+        signalRight = np.load(f)
     
+    
+    featureExtractor = FeatureExtractor(method="psd", sample_rate=100.) #instanciamos el extractor de características
+    featuresleft = featureExtractor.fit_transform(signalLeft) #signal [n_channels, n_samples, n_trials]
+
+    featureExtractor = FeatureExtractor(method="psd", sample_rate=100.) #instanciamos el extractor de características
+    featuresright = featureExtractor.fit_transform(signalRight) #signal [n_channels, n_samples, n_trials]
+
+    c3, cz, c4 = 26, 28, 30 #canales de interés
 
     import matplotlib.pyplot as plt
-    plt.plot(featureExtractor.freqs, features[0,:])
+    plt.title("Potencia de la señal en la banda alfa y beta - Promedio trials - Clase $left$")
+    plt.plot(featureExtractor.freqs, featuresleft[c3].mean(axis=1), label = "C3")
+    plt.plot(featureExtractor.freqs, featuresleft[cz].mean(axis=1), label = "Cz")
+    plt.plot(featureExtractor.freqs, featuresleft[c4].mean(axis=1), label = "C4")
+    plt.xlim(1,30)
+    plt.legend()
+    plt.grid()
     plt.show()
 
-    filtro = featureExtractor.freqs>=8
-    filtro2 = featureExtractor.freqs<=30
+    fig, ax = plt.subplots(1,3, figsize=(10,5))
+    fig.suptitle("Potencia de la señal en las bandas alfa (mu) y beta - Promedio trials - Clase $left$ y $right$")
+    ax[0].plot(featureExtractor.freqs, featuresleft[c3].mean(axis=1), label = "left")
+    ax[0].plot(featureExtractor.freqs, featuresright[c3].mean(axis=1), label = "right")
+    ax[0].set_title("C3")
+    ax[0].grid()
+    ax[0].set_xlim(1,30)
+    ax[0].legend()
+    ax[1].plot(featureExtractor.freqs, featuresleft[cz].mean(axis=1), label = "left")
+    ax[1].plot(featureExtractor.freqs, featuresright[cz].mean(axis=1), label = "right")
+    ax[1].set_title("Cz")
+    ax[1].grid()
+    ax[1].set_xlim(1,30)
+    ax[1].legend()
+    ax[2].plot(featureExtractor.freqs, featuresleft[c4].mean(axis=1), label = "left")
+    ax[2].plot(featureExtractor.freqs, featuresright[c4].mean(axis=1), label = "right")
+    ax[2].set_title("C4")
+    ax[2].grid()
+    ax[2].set_xlim(1,30)
+    ax[2].legend()
+    plt.show()
 
-    features[:,(filtro)&(filtro2)]
-
-
-
-
-    with open("testing_features.npy", "wb") as f:
-        np.save(f, features)
+    with open("testing_left_features.npy", "wb") as f:
+        np.save(f, featuresleft)
