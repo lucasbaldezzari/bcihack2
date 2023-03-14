03f6043898ce7c4eb1f88afd1c00f39f16bc8975
lucasbaldezzari <lmbaldezzari@gmail.com>
2023-03-14 18:28:07 -0300

    Se trabaja sobre Filter.py. Se mueven archivos. Se trabaja sobre test.py para probar las clases Filter, CSPMulticlass, RavelTransfomer y FeatureExtractor junto con un LDA utilizando grid_search y otras clases de scikitlearn

--- Desarrollo/PythonScripts/scripts/SignalProcessor/Filter.py
+++ Desarrollo/PythonScripts/scripts/SignalProcessor/Filter.py
@@ -7,20 +7,16 @@ class Filter(BaseEstimator, TransformerMixin):
     [canales, muestras]. La idea es aplicar un filtro pasa banda y un filtro notch a la señal a todo el array.
     La clase se puede usar como un objeto de sklearn, por lo que se puede usar en un pipeline de sklearn."""
 
-    def __init__(self):
-        """Inicializa el objeto con los parámetros de filtrado."""
-        pass
-
-    def fit(self, X = None, y=None, lowcut = 8.0, highcut = 30.0, notch_freq = 50.0, notch_width = 2.0, sample_rate = 250.0
-            , axisToCompute = 2):
-        """Creamos los filtros.
+    def __init__(self, lowcut = 8.0, highcut = 30.0, notch_freq = 50.0, notch_width = 2.0, sample_rate = 250.0,
+                 axisToCompute = 2):
+        """Inicializa el objeto con los parámetros de filtrado.
         -lowcut: Frecuencia de corte inferior del filtro pasa banda.
         -highcut: Frecuencia de corte superior del filtro pasa banda.
         -notch_freq: Frecuencia de corte del filtro notch.
         -notch_width: Ancho de banda del filtro notch.
         -sample_rate: Frecuencia de muestreo de la señal.
         -X: Señal de entrada. No se usa en este caso.
-        -y: No se usa en este caso."""""
+        -y: No se usa en este caso."""
 
         self.lowcut = lowcut
         self.highcut = highcut
@@ -32,34 +28,41 @@ class Filter(BaseEstimator, TransformerMixin):
         self.b, self.a = butter(5, [self.lowcut, self.highcut], btype='bandpass', fs=self.sample_rate)
         self.b_notch, self.a_notch = iirnotch(self.notch_freq, self.notch_width, self.sample_rate)
 
+    def fit(self, X = None, y=None):
+        """Métod fit. No hace nada."""
+
         return self #el método fit siempre debe retornar self!
 
-    def transform(self, signal):
+    def transform(self, signal, y = None):
         """Función para aplicar los filtros a la señal.
         -signal: Es la señal en un numpy array de la forma [n-trials, canales, muestras]."""
 
-        signal = np.subtract(signal, signal.mean(axis=0)) #restamos la media de cada muestra
-        signal = filtfilt(self.b, self.a, signal, axis=1) #aplicamos el filtro pasa banda
+        # signal = np.subtract(signal, signal.mean(axis = self.axisToCompute)) #restamos la media de cada muestra
+        signal = signal - np.mean(signal, axis=self.axisToCompute, keepdims=True)
+        signal = filtfilt(self.b, self.a, signal, axis = self.axisToCompute) #aplicamos el filtro pasa banda
         signal = filtfilt(self.b_notch, self.a_notch, signal, axis = self.axisToCompute) #aplicamos el filtro notch
         return signal
 
-
 if __name__ == "__main__":
 
-    with open('all_left_trials.npy', 'rb') as f:
-        signal = np.load(f)
+    with open('testData/all_left_trials.npy', 'rb') as f:
+        signalleft = np.load(f)
 
-    filtro = Filter()
-    filtro.fit(lowcut=1.0, highcut=36.0, notch_freq=50.0, notch_width=2.0, sample_rate=250.0)
-    signalFiltered = filtro.transform(signal)
+    with open('testData/all_right_trials.npy', 'rb') as f:
+        signalright = np.load(f)
+
+    filtro = Filter(lowcut=8.0, highcut=28.0, notch_freq=50.0, notch_width=2.0, sample_rate=100.0)
+
+    signalleftFiltered = filtro.fit_transform(signalleft)
+    sognalrightFiltered = filtro.fit_transform(signalright)
 
     # signalFiltered = signalpros.fit_transform(signal,highcut=36.0, notch_freq=50.0, notch_width=2.0)
 
     ### Grafico para comparar señal original y señal filtrada
     import matplotlib.pyplot as plt
-    plt.plot(signal[11,:])
-    plt.plot(signalFiltered[11,:])
+    plt.plot(signalleftFiltered[0,28,:], label = "Filtrada")
+    plt.plot(signalleft[0,28,:], label = "Original")
     plt.show()
 
-    with open("testsignal_filtered.npy", "wb") as f:
-        np.save(f, signalFiltered)
+    # with open("testsignal_filtered.npy", "wb") as f:
+    #     np.save(f, signalFiltered)
--- Desarrollo/PythonScripts/scripts/test.py
+++ Desarrollo/PythonScripts/scripts/test.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 # from SignalProcessor import Filter, FeatureExtractor, Classifier
 from SignalProcessor.Filter import Filter
 from SignalProcessor.FeatureExtractor import FeatureExtractor
-from SignalProcessor.Classifier import Classifier
+from SignalProcessor.CSPMulticlass import CSPMulticlass
+from SignalProcessor.RavelTransformer import RavelTransformer
 
 from tools import get_names, prepareData, getTrials, plot_signals
 
@@ -14,7 +15,7 @@ from tools import get_names, prepareData, getTrials, plot_signals
 #Descripción de los datos en: https://bbci.de/competition/iv/desc_1.html
 #IMPORTANTE!! Leer el "Experimental Setup" en https://bbci.de/competition/iv/desc_1.html
 
-path = "__datasetsForTesting/"
+path = "SignalProcessor/testData/"
 
 files = get_names(path)
 
@@ -27,8 +28,10 @@ datoslistos = prepareData(datos)
 ## A continuación haremos un análisis considerando sólo los datos del sujeto 1
 sujeto1 = datoslistos["subject1"]
 
-eeg = sujeto1["eeg"]
+#extraemos el eeg
+eeg = sujeto1["eeg"] #eeg sin filtrar
 
+#imprimios algunos datos
 print("Cantidad de datos de EEG: ", eeg.shape)
 print("Frecuencia de muestreo: ", sujeto1["sample_rate"])
 print("Número de canales:", sujeto1["nchannels"])
@@ -40,56 +43,152 @@ print("Cantidad de clases:", sujeto1["nclasses"])
 print("La clase 1 es:", sujeto1["class1"])
 print("La clase 2 es:", sujeto1["class2"])
 
+#algunos canales de interés
 channels = ["C3", "Cz", "C4"]
 c3 = sujeto1["channelsNames"].index("C3")
 cz = sujeto1["channelsNames"].index("Cz")
 c4 = sujeto1["channelsNames"].index("C4")
 
-# eeg = eeg[[c3, cz, c4], :] #nos quedamos solo con los canales C3, Cz y C4
-
-#Aplicamos filtro a la señal
-filtro = Filter()
-eeg_filtered = filtro.fit_transform(eeg, lowcut = 6.0, highcut = 30.0, notch_freq = 50.0, notch_width = 2.0, sample_rate = 100, axisToCompute = 1)
-
-#Gráficamos un trozo de las señales filtradas y no filtradas
-plot_signals([eeg[:, 0:1000][0], eeg_filtered[:, 0:1000][0]], title="Señales filtradas y no filtradas",
-             legend=["Señal no filtrada", "Señal filtrada"])
-
 #Dividimos la señal en trials considerando los event_starting
-trials = getTrials(eeg_filtered, [sujeto1["class1"], sujeto1["class2"]], sujeto1["event_codes"], sujeto1["event_starting"], 59,
+trials = getTrials(eeg, [sujeto1["class1"], sujeto1["class2"]], sujeto1["event_codes"], sujeto1["event_starting"], 59,
                    w1=-0.5, w2=2.5, sample_rate=100)
-
-# with open("all_left_trials.npy", "wb") as f:
-#     np.save(f, trials["left"])
-
-# with open("all_right_trials.npy", "wb") as f:
-#     np.save(f, trials["right"])
 
 clase1 = sujeto1["class1"]
 clase2 = sujeto1["class2"]
 print("La forma de los trials de la clase", clase1, "es:", trials[clase1].shape)
 print("La forma de los trials de la clase", clase2, "es:", trials[clase2].shape)
 
-#Graficamos un trial de cada clase para un canal
-plot_signals([trials[clase1][0, 0, :], trials[clase2][0, 0, :]], title="Señales de la clase 1 y 2",
-                legend=["Señal de la clase 1", "Señal de la clase 2"])
+# #saving the trials in npy files for each class
+# np.save("SignalProcessor/testData/noisy_eeg_classLeft.npy", trials[clase1])
+# np.save("SignalProcessor/testData/noisy_eeg_classRight.npy", trials[clase2])
 
-#Extraemos las características de los trials usando FeatureExtractor.py
-featureExtractor = FeatureExtractor(method = "welch", sample_rate = 100, axisToCompute = 2)
+#Graficamos el trial 50 de la clase 1 y la clase 2 para el canal c3
+t1 = -0.5
+t2 = 2.5
+sample_rate = 100
+tline = np.arange(t1, t2, 1/sample_rate)
 
-featurescl1 = featureExtractor.fit_transform(trials[clase1])
-featurescl2 = featureExtractor.fit_transform(trials[clase2])
+## Concatemaos los trials de las dos clases
+#Contactemos los trials de cada clase en un sólo array
+eegmatrix = np.concatenate((trials[clase1],trials[clase2]), axis=0) #importante el orden con el que concatenamos
+print(eegmatrix.shape) #[ n_trials (o n_epochs), n_channels, n_samples]
+
+## generamos las labels para cada clase
+class_info = {1: "left", 2: "right"} #diccionario para identificar clases. El orden se corresponde con lo que hay eneegmatrix
+n_clases = len(list(class_info.keys()))
+
+#genero las labels
+n_trials = trials[clase1].shape[0]
+totalTrials = eegmatrix.shape[0]
+labels = np.array([np.full(n_trials, label) for label in class_info.keys()]).reshape(totalTrials)
+print(labels.shape)
+# print(labels) #las labels se DEBEN corresponder con el orden de los trials en eegmatrix
 
 
-#Plotting the features using plot_signals for one trial and one channel
-plot_signals([featurescl1[0, 0, :], featurescl2[0, 0, :]], title="Características de la clase 1 y 2 - Hilbert",
-                legend=["Features Cl1", "Features Cl1"])
+"""
+*************** PIPELINE Y CLASIFICACIÓN ***************
+Vamos a generar un pipeline para filtrar la señal, extraer características y clasificar.
+"""
 
-#Extraemos características usando psd
-featureExtractor = FeatureExtractor(method = "psd")
-featurescl1_psd = featureExtractor.fit_transform(trials[clase1])
-featurescl2_psd = featureExtractor.fit_transform(trials[clase2])
+#Separamos los datos en train, test y validación
+from sklearn.model_selection import train_test_split, GridSearchCV
 
-#Plotting the features using plot_signals for one trial and one channel
-plot_signals([featurescl1_psd[0, 0, :], featurescl2_psd[0, 0, :]], title="Características de la clase 1 y 2 - PSD",
-                legend=["Features Cl1", "Features Cl1"])
+eeg_train, eeg_test, labels_train, labels_test = train_test_split(eegmatrix, labels, test_size=0.2, random_state=1)
+eeg_train, eeg_val, labels_train, labels_val = train_test_split(eeg_train, labels_train, test_size=0.2, random_state=1)
+
+## ******************************
+## Creamos el pipeline
+## ******************************
+
+#Creamos un filtro pasabanda
+pasabanda = Filter(lowcut=8.0, highcut=28.0, notch_freq=50.0, notch_width=2.0, sample_rate=100.0)
+
+## Probamos el pasabanda
+# eeg_train_filtered = pasabanda.fit_transform(eeg_train)
+
+# ## graficamos el trial 1 de la clase 1 y la clase 2 para el canal c3 antes y después de filtrar
+# plt.plot(tline,eeg_train[1, c3, :], label="Sin filtrar")
+# plt.plot(tline,eeg_train_filtered[1, c3, :], label="Filtrado")
+# plt.legend()
+# plt.xlabel("Tiempo (s)")
+# plt.ylabel("Amplitud (uV)")
+# plt.title("Señales de EEG para el canal C3 (filtrado)")
+# plt.show()
+
+#Creamos un CSPMulticlass - Método ovo (one vs one)
+cspmulticlass = CSPMulticlass(n_components=2, method = "ovo", n_classes = len(np.unique(labels)), reg=None, log=None, norm_trace=False)
+print(f"Cantidad de filtros CSP a entrenar: {len(cspmulticlass.csplist)}")
+
+#Creamos un FeatureExtractor
+featureExtractor = FeatureExtractor(method = "welch", sample_rate=100., axisToCompute=2)
+
+#Transoformer para acomodar los datos de [n_trials, n_channels, n_samples] a [n_trials, n_componentes x n_samples] = [n_trials, n_features]
+ravelTransformer = RavelTransformer()
+
+#creamos el pipeline con un pasabanda, un cspmulticlase, un featureExtractor y un LDA
+from sklearn.pipeline import Pipeline
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+pipeline = Pipeline([
+    ('pasabanda', pasabanda),
+    ('cspmulticlase', cspmulticlass),
+    ('featureExtractor', featureExtractor),
+    ('ravelTransformer', ravelTransformer),
+    ('lda', LinearDiscriminantAnalysis())
+])
+
+"""Análisis rápido con el pipeline"""
+pipeline.fit(eeg_train, labels_train)
+print(pipeline.score(eeg_test, labels_test))
+
+"""Búsqueda de hiperparámetros con GridSearchCV"""
+#Definimos una grilla de parámetros para el GridSearch
+param_grid = {
+    'pasabanda__lowcut': [8.0, 9.0],
+    'pasabanda__highcut': [15.0, 28.0],
+    'cspmulticlase__n_components': [2],
+    'featureExtractor__method': ['hilbert','welch'],
+    'lda__solver': ['svd', 'lsqr', 'eigen'],
+    'lda__shrinkage': ['auto', None]
+    }
+
+#Creamos el GridSearch
+grid = GridSearchCV(pipeline, param_grid, cv=5, n_jobs=-1)
+grid.fit(eeg_train, labels_train)
+
+#Nos quedamos con el mejor estimador
+best_estimator = grid.best_estimator_
+
+#Usamos el mejor estimador para predecir sobre los datos de test
+best_estimator.fit(eeg_test, labels_test)
+
+#Usamos el mejor estimador para predecir sobre los datos de validación
+y_pred = best_estimator.predict(eeg_val)
+
+#Calculamos la matriz de confusión
+from sklearn.metrics import confusion_matrix
+confusion_matrix(labels_val, y_pred)
+
+#Calculamos el accuracy
+from sklearn.metrics import accuracy_score, classification_report
+print(accuracy_score(labels_val, y_pred))
+
+#guardamos el mejor modelo con los hiparámetros encontrados en la carpeta models. Si no existe la carpeta, la creamos
+import os
+if not os.path.exists('models'):
+    os.makedirs('models')
+
+import pickle
+filename = 'models/best_model.sav'
+pickle.dump(best_estimator, open(filename, 'wb'))
+
+#usamos el modelo para predecir sobre un nuevo trial (usando un trial de la clase 1 de los datos de validación)
+trial = eeg_val[9].reshape(1, eeg_val.shape[1], eeg_val.shape[2])
+print(trial.shape)
+
+#predecimos
+y_pred = best_estimator.predict(trial)
+print(y_pred)
+
+
+
