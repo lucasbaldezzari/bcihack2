4e71022440e54c3504cc90d81b25c931def0bd1a
lucasbaldezzari <lmbaldezzari@gmail.com>
2023-03-14 09:21:46 -0300

    Se actualiza FeatureExtractor aagregando Hilbert

--- Desarrollo/PythonScripts/scripts/SignalProcessor/FeatureExtractor.py
+++ Desarrollo/PythonScripts/scripts/SignalProcessor/FeatureExtractor.py
@@ -1,22 +1,21 @@
 import numpy as np
 from scipy.signal import hilbert, welch
 from sklearn.base import BaseEstimator, TransformerMixin
-from matplotlib import mlab
+# from matplotlib import mlab
 
 class FeatureExtractor(BaseEstimator, TransformerMixin):
     """La clase para extraer características de desincronización relacionada con eventos (ERD) y sincronización relacionada con eventos (ERS) de señales EEG.
-    La idea es usar la transformada de Hilbert para obtener la señal analítica y luego calcular la potencia de la señal en las bandas
-    alfa y beta. La potencia en la banda alfa es la ERD y la potencia en la banda beta es la ERS.
-    La clase se puede usar como un objeto de sklearn, por lo que se puede usar en un pipeline de sklearn.
-    
-    Comentario: Esta es una primera versión. Se debe probar otras estrategias para que la extracción de características sea más robusta.
-    Por ejemplo, se puede implementar ICA para eliminar las componentes de ruido de la señal y luego aplicar la transformada de Hilbertm, o bien
-    implementar CSP para extraer las componentes de interés de la señal y luego aplicar la transformada de Hilbert."""
+    - La clase puede extraer la envolvente de la señal a partir de Hilber, o,
+    - La clase puede extraer la potencia de la señal a partir de la transformada de Welch.
+
+    La clase se comporta como un transformer de sklearn, por lo que puede ser usada en un pipeline de sklearn.
+    """
 
     def __init__(self, method = "welch", sample_rate = 250., axisToCompute = 2):
         """No se inicializan atributos.
-        - method: método por el cual extraer la potencia
-        - overlap: solapamiento en segundos"""
+        - method: método por el cual extraer las caracerísticas. Puede ser welch o hilbert.
+        - sample_rate: frecuencia de muestreo de la señal.
+        - axisToCompute: eje a lo largo del cual se calculará la transformada."""
 
         self.method = method
         self.sample_rate = sample_rate
@@ -35,11 +34,16 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
         
         if self.method == "welch":
             """Retorna la potencia de la señal en la forma [n_trials, canales, power_samples]"""
-            #Aplicamos la transformada de Hilbert
             self.freqs, self.power = welch(signal, axis=self.axisToCompute) #trnasformada de Welch
             
             self.freqs = self.freqs*self.sample_rate
             return self.power
+        
+        if self.method == "hilbert":
+            """Retorna la potencia de la señal en la forma [n_trials, canales, power_samples]"""
+            analyticSignal = hilbert(signal, axis=self.axisToCompute)
+            self.envolvente = np.abs(analyticSignal) #envolvente de la señal analítica
+            return self.envolvente
         
     def fit_transform(self, signal, y = None):
         """Función para aplicar los filtros a la señal.
@@ -50,51 +54,80 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
 
 if __name__ == '__main__':
 
-    with open("all_left_trials.npy", "rb") as f:
+    with open("testData/all_left_trials.npy", "rb") as f:
         signalLeft = np.load(f)
 
-    with open("all_right_trials.npy", "rb") as f:
+    with open("testData/all_right_trials.npy", "rb") as f:
         signalRight = np.load(f)
     
-    signalLeft.shape
-    
+
+    ## Extraemos envolventes de las señals
+    featureExtractor = FeatureExtractor(method="hilbert", sample_rate=100.) #instanciamos el extractor de características
+    featuresleft = featureExtractor.fit_transform(signalLeft) #signal [n_channels, n_samples, n_trials]
+
+    featureExtractor = FeatureExtractor(method="hilbert", sample_rate=100.) #instanciamos el extractor de características
+    featuresright = featureExtractor.fit_transform(signalRight) #signal [n_channels, n_samples, n_trials]
+
+    c3, cz, c4 = 26, 28, 30 #canales de interés
+
+    t1 = -0.5
+    t2 = 2.5
+    sample_rate = 100.
+    timeline = np.arange(t1, t2, 1/sample_rate)
+    timeline.shape
+
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots(1,3, figsize=(10,5))
+    fig.suptitle("Envolvente de la señal - Promedio trials - Clases $left$ y $right$")
+    ax[0].plot(timeline, featuresleft.mean(axis=0)[c3], label = "left")
+    ax[0].plot(timeline, featuresright.mean(axis = 0)[c3], label = "right")
+    ax[0].set_title("C3")
+    ax[0].set_xlabel("Tiempo (s)")
+    ax[0].legend()
+    ax[0].grid()
+    ax[1].plot(timeline, featuresleft.mean(axis=0)[cz], label = "left")
+    ax[1].plot(timeline, featuresright.mean(axis = 0)[cz], label = "right")
+    ax[1].set_title("Cz")
+    ax[1].set_xlabel("Tiempo (s)")
+    ax[1].legend()
+    ax[1].grid()
+    ax[2].plot(timeline, featuresleft.mean(axis=0)[c4], label = "left")
+    ax[2].plot(timeline, featuresright.mean(axis = 0)[c4], label = "right")
+    ax[2].set_title("C4")
+    ax[2].set_xlabel("Tiempo (s)")
+    ax[2].legend()
+    ax[2].grid()
+    plt.show()
+
+
+    ## Extraemos potencia de las señales
     featureExtractor = FeatureExtractor(method="welch", sample_rate=100.) #instanciamos el extractor de características
     featuresleft = featureExtractor.fit_transform(signalLeft) #signal [n_channels, n_samples, n_trials]
 
     featureExtractor = FeatureExtractor(method="welch", sample_rate=100.) #instanciamos el extractor de características
     featuresright = featureExtractor.fit_transform(signalRight) #signal [n_channels, n_samples, n_trials]
 
-    c3, cz, c4 = 26, 28, 30 #canales de interés
-
-    import matplotlib.pyplot as plt
-    plt.title("Potencia de la señal en la banda alfa y beta - Promedio trials - Clase $left$")
-    plt.plot(featureExtractor.freqs, featuresleft.mean(axis=0)[c3], label = "C3")
-    plt.plot(featureExtractor.freqs, featuresleft.mean(axis=0)[cz], label = "Cz")
-    plt.plot(featureExtractor.freqs, featuresleft.mean(axis=0)[c4], label = "C4")
-    plt.xlim(1,30)
-    plt.legend()
-    plt.grid()
-    plt.show()
 
     fig, ax = plt.subplots(1,3, figsize=(10,5))
-    fig.suptitle("Potencia de la señal en las bandas alfa (mu) y beta - Promedio trials - Clase $left$ y $right$")
+    fig.suptitle("Potencia de la señal en las bandas alfa (mu) y beta - Promedio trials - Clases $left$ y $right$")
     ax[0].plot(featureExtractor.freqs, featuresleft.mean(axis=0)[c3], label = "left")
     ax[0].plot(featureExtractor.freqs, featuresright.mean(axis=0)[c3], label = "right")
     ax[0].set_title("C3")
     ax[0].grid()
-    ax[0].set_xlim(1,30)
+    ax[0].set_xlim(8,30)
     ax[0].legend()
     ax[1].plot(featureExtractor.freqs, featuresleft.mean(axis=0)[cz], label = "left")
     ax[1].plot(featureExtractor.freqs, featuresright.mean(axis=0)[cz], label = "right")
     ax[1].set_title("Cz")
     ax[1].grid()
-    ax[1].set_xlim(1,30)
+    ax[1].set_xlim(8,30)
     ax[1].legend()
     ax[2].plot(featureExtractor.freqs, featuresleft.mean(axis=0)[c4], label = "left")
     ax[2].plot(featureExtractor.freqs, featuresright.mean(axis=0)[c4], label = "right")
     ax[2].set_title("C4")
     ax[2].grid()
-    ax[2].set_xlim(1,30)
+    ax[2].set_xlim(8,30)
     ax[2].legend()
     plt.show()
 
