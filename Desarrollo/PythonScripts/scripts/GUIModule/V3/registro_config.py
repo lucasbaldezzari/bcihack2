0ddf80e7bd232961cbf6c3de4215bde53a0565fb
emito121 <emiliano.alvarez@estudiantes.utec.edu.uy>
2023-04-14 00:04:43 -0300

    Se soluciona issue. Se agrega ejemplo de como se podria actualizar la interfaz por medio de QTimer en el simular core

---
+++ Desarrollo/PythonScripts/scripts/GUIModule/V3/registro_config.py
@@ -0,0 +1,106 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import numpy as np
+from PyQt5 import uic
+import sys
+import pickle
+import json
+
+class MainWindow(QDialog):
+
+    def __init__(self, gui2, fileName):
+        super().__init__()
+        uic.loadUi("config_registro.ui", self)
+
+        self.entrenamiento = gui2
+        self.fileName = fileName
+        self.btn_iniciar.clicked.connect(self.Inicio) #Guarda en .json e inicia la interfaz de entrenamiento
+        self.btn_regresar.clicked.connect(self.Cerrar) #Cierra la interfaz
+        self.btn_guardar.clicked.connect(self.Guardar) #Solo reescribe el .json
+        self.Cargar() #establece los valores al inicio en base al archivo .json
+
+    def Cargar(self):
+        with open(self.fileName, 'r') as fp:
+            configParameters = eval(fp.read())
+            self.desplegable_sesion.setCurrentIndex(configParameters["typeSesion"])
+            self.desplegable_paradigma.setCurrentIndex(configParameters["cueType"])
+            self.line_trials.setText(f'{configParameters["ntrials"]}')
+            self.line_valorclases.setText(f'{configParameters["classes"]}')
+            self.line_nombreclases.setText(f'{configParameters["clasesNames"]}')
+            self.line_inicio.setText(f'{configParameters["startingTimes"]}')
+            self.line_tiempo_accion.setText(f'{configParameters["cueDuration"]}')
+            self.line_tiempo_descanso.setText(f'{configParameters["finishDuration"]}')
+            self.line_tiempoClasif.setText(f'{configParameters["lenToClassify"]}')
+            self.line_sujeto.setText(f'{configParameters["subjectName"]}')
+            self.line_sesion.setText(f'{configParameters["sesionNumber"]}')
+
+            #Parámetros para inicar la placa openbci
+            self.desplegable_placa.setCurrentText(configParameters["boardParams"]['boardName'])
+            self.line_puerto.setText(f'{configParameters["boardParams"]["serialPort"]}')
+
+            #parámetros del filtro
+            self.line_lowpass.setText(f'{configParameters["filterParameters"]["lowcut"]}')
+            self.line_highpass.setText(f'{configParameters["filterParameters"]["highcut"]}')
+            self.line_notch.setText(f'{configParameters["filterParameters"]["notch_freq"]}')
+            self.line_anchonotch.setText(f'{configParameters["filterParameters"]["notch_width"]}')
+            self.line_frecuencia.setText(f'{configParameters["filterParameters"]["sample_rate"]}')
+            self.line_axis.setText(f'{configParameters["filterParameters"]["axisToCompute"]}')
+
+            self.desplegable_extractor.setCurrentText(configParameters['featureExtractorMethod'])
+
+            self.line_csp.setText(f'{configParameters["cspFile"]}')
+            self.line_clasificador.setText(f'{configParameters["classifierFile"]}')
+
+    def Guardar(self):
+        with open(self.fileName, 'w') as fp:
+            configParameters = dict()
+            configParameters["typeSesion"] = int(self.desplegable_sesion.findText(self.desplegable_sesion.currentText()))
+            configParameters["cueType"] = int(self.desplegable_paradigma.findText(self.desplegable_paradigma.currentText()))
+            configParameters["ntrials"] = int(self.line_trials.text())
+            configParameters["classes"] = eval(self.line_valorclases.text())
+            configParameters["clasesNames"] = eval(self.line_nombreclases.text())
+            configParameters["startingTimes"] = eval(self.line_inicio.text())
+            configParameters["cueDuration"]= float(self.line_tiempo_accion.text())
+            configParameters["finishDuration"] = float(self.line_tiempo_descanso.text())
+            configParameters["lenToClassify"] = float(self.line_tiempoClasif.text())
+            configParameters["subjectName"] = self.line_sujeto.text()
+            configParameters["sesionNumber"] = int(self.line_sesion.text())
+
+            #Parámetros para inicar la placa openbci
+            configParameters["boardParams"] = dict()
+            configParameters["boardParams"]['boardName'] = self.desplegable_placa.currentText()
+            configParameters["boardParams"]["serialPort"] = self.line_puerto.text()
+
+            #parámetros del filtro
+            configParameters["filterParameters"] = dict()
+            configParameters["filterParameters"]["lowcut"] = float(self.line_lowpass.text())
+            configParameters["filterParameters"]["highcut"] = float(self.line_highpass.text())
+            configParameters["filterParameters"]["notch_freq"] = float(self.line_notch.text())
+            configParameters["filterParameters"]["notch_width"] = float(self.line_notch.text())
+            configParameters["filterParameters"]["sample_rate"] = float(self.line_frecuencia.text())
+            configParameters["filterParameters"]["axisToCompute"] = int(self.line_axis.text())
+
+            configParameters['featureExtractorMethod'] = self.desplegable_extractor.currentText()
+
+            configParameters["cspFile"] = self.line_csp.text()
+            configParameters["classifierFile"] = self.line_clasificador.text()
+
+            json.dump(configParameters, fp)
+
+    def Inicio(self):
+
+        self.Guardar()
+
+        self.close()
+
+        self.entrenamiento.show()
+
+    def Cerrar(self):
+        self.close()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    _ventana = MainWindow()
+    _ventana.show()
+    app.exec_()
