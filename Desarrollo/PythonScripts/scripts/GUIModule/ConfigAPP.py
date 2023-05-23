9324d6a659a1dabd5956dccb7f1e5cd12b34d21c
emito121 <emiliano.alvarez@estudiantes.utec.edu.uy>
2023-05-22 23:36:22 -0300

    Realizo cambios en la ConfigAPP

--- Desarrollo/PythonScripts/scripts/GUIModule/ConfigAPP.py
+++ Desarrollo/PythonScripts/scripts/GUIModule/ConfigAPP.py
@@ -1,7 +1,6 @@
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-import numpy as np
 from PyQt5 import uic
 import sys
 import json
@@ -37,6 +36,7 @@ class ConfigAPP(QDialog):
 
             #Parámetros para inicar la placa openbci
             self.desplegable_placa.setCurrentText(configParameters["boardParams"]['boardName'])
+            self.line_canales.setText(f'{configParameters["boardParams"]["channels"]}')
             self.line_puerto.setText(f'{configParameters["boardParams"]["serialPort"]}')
 
             #parámetros del filtro
@@ -79,7 +79,7 @@ class ConfigAPP(QDialog):
             #Parámetros para inicar la placa openbci
             configParameters["boardParams"] = dict()
             configParameters["boardParams"]['boardName'] = self.desplegable_placa.currentText()
-            configParameters["boardParams"]['channels'] = [0,2,3] #ESTA LINEA HAY QUE ADAPTARLA A LA APP DE CONFIG
+            configParameters["boardParams"]['channels'] = eval(self.line_canales.text()) 
             configParameters["boardParams"]["serialPort"] = self.line_puerto.text()
 
             #parámetros del filtro
@@ -121,8 +121,8 @@ class ConfigAPP(QDialog):
     def Cerrar(self):
         self.close()
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    _ventana = ConfigAPP("config.json")
-    _ventana.show()
-    app.exec_()
+# if __name__ == "__main__":
+app = QApplication(sys.argv)
+_ventana = ConfigAPP("config.json")
+_ventana.show()
+app.exec_()
