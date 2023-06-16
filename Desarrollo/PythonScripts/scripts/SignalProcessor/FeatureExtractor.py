29f4de38bdc7bf7acf6846631429595f92154c1b
lucasbaldezzari <lmbaldezzari@gmail.com>
2023-06-16 17:26:48 -0300

    Se modifica FeatureExtractor. Se realizan análisis sobre datos de prueba. Hay que evaluar el funcionamiento de CSPMulticlass

--- Desarrollo/PythonScripts/scripts/SignalProcessor/FeatureExtractor.py
+++ Desarrollo/PythonScripts/scripts/SignalProcessor/FeatureExtractor.py
@@ -11,17 +11,20 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
     La clase se comporta como un transformer de sklearn, por lo que puede ser usada en un pipeline de sklearn.
     """
 
-    def __init__(self, method = "welch", sample_rate = 250., axisToCompute = 2):
+    def __init__(self, method = "welch", sample_rate = 250., axisToCompute = 2, band_values = None):
         """No se inicializan atributos.
         - method: método por el cual extraer las caracerísticas. Puede ser welch o hilbert.
         - sample_rate: frecuencia de muestreo de la señal.
-        - axisToCompute: eje a lo largo del cual se calculará la transformada."""
-        
+        - axisToCompute: eje a lo largo del cual se calculará la transformada.
+        - utils_freqs: lista de dos valores con el 
+        rango de freucencias a devolver cuando se aplique transform o fit_transform"""
+
         assert method in ["hilbert", "welch"], "method debe ser 'hilbert' o 'welch'"
         self.method = method
         self.sample_rate = sample_rate
         self.axisToCompute = axisToCompute
         self.band_values = band_values
+        
 
     def fit(self, X = None, y=None):
         """No hace nada"""
@@ -61,85 +64,17 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
 
         self.fit()
         return self.transform(signal)
+    
 
 if __name__ == '__main__':
 
-    with open("testData/all_left_trials.npy", "rb") as f:
+    with open("SignalProcessor\TestData\All_left_trials.npy", "rb") as f:
         signalLeft = np.load(f)
 
-    with open("testData/all_right_trials.npy", "rb") as f:
+    with open("SignalProcessor\TestData\All_right_trials.npy", "rb") as f:
         signalRight = np.load(f)
     
 
     ## Extraemos envolventes de las señals
-    featureExtractor = FeatureExtractor(method="hilbert", sample_rate=100.) #instanciamos el extractor de características
+    featureExtractor = FeatureExtractor(method="welch", sample_rate=100., band_values = [8,12]) #instanciamos el extractor de características
     featuresleft = featureExtractor.fit_transform(signalLeft) #signal [n_channels, n_samples, n_trials]
-
-    featureExtractor = FeatureExtractor(method="hilbert", sample_rate=100.) #instanciamos el extractor de características
-    featuresright = featureExtractor.fit_transform(signalRight) #signal [n_channels, n_samples, n_trials]
-
-    c3, cz, c4 = 26, 28, 30 #canales de interés
-
-    t1 = -0.5
-    t2 = 2.5
-    sample_rate = 100.
-    timeline = np.arange(t1, t2, 1/sample_rate)
-    timeline.shape
-
-    import matplotlib.pyplot as plt
-
-    fig, ax = plt.subplots(1,3, figsize=(10,5))
-    fig.suptitle("Envolvente de la señal - Promedio trials - Clases $left$ y $right$")
-    ax[0].plot(timeline, featuresleft.mean(axis=0)[c3], label = "left")
-    ax[0].plot(timeline, featuresright.mean(axis = 0)[c3], label = "right")
-    ax[0].set_title("C3")
-    ax[0].set_xlabel("Tiempo (s)")
-    ax[0].legend()
-    ax[0].grid()
-    ax[1].plot(timeline, featuresleft.mean(axis=0)[cz], label = "left")
-    ax[1].plot(timeline, featuresright.mean(axis = 0)[cz], label = "right")
-    ax[1].set_title("Cz")
-    ax[1].set_xlabel("Tiempo (s)")
-    ax[1].legend()
-    ax[1].grid()
-    ax[2].plot(timeline, featuresleft.mean(axis=0)[c4], label = "left")
-    ax[2].plot(timeline, featuresright.mean(axis = 0)[c4], label = "right")
-    ax[2].set_title("C4")
-    ax[2].set_xlabel("Tiempo (s)")
-    ax[2].legend()
-    ax[2].grid()
-    plt.show()
-
-
-    ## Extraemos potencia de las señales
-    featureExtractor = FeatureExtractor(method="welch", sample_rate=100.) #instanciamos el extractor de características
-    featuresleft = featureExtractor.fit_transform(signalLeft) #signal [n_channels, n_samples, n_trials]
-
-    featureExtractor = FeatureExtractor(method="welch", sample_rate=100.) #instanciamos el extractor de características
-    featuresright = featureExtractor.fit_transform(signalRight) #signal [n_channels, n_samples, n_trials]
-
-
-    fig, ax = plt.subplots(1,3, figsize=(10,5))
-    fig.suptitle("Potencia de la señal en las bandas alfa (mu) y beta - Promedio trials - Clases $left$ y $right$")
-    ax[0].plot(featureExtractor.freqs, featuresleft.mean(axis=0)[c3], label = "left")
-    ax[0].plot(featureExtractor.freqs, featuresright.mean(axis=0)[c3], label = "right")
-    ax[0].set_title("C3")
-    ax[0].grid()
-    ax[0].set_xlim(8,30)
-    ax[0].legend()
-    ax[1].plot(featureExtractor.freqs, featuresleft.mean(axis=0)[cz], label = "left")
-    ax[1].plot(featureExtractor.freqs, featuresright.mean(axis=0)[cz], label = "right")
-    ax[1].set_title("Cz")
-    ax[1].grid()
-    ax[1].set_xlim(8,30)
-    ax[1].legend()
-    ax[2].plot(featureExtractor.freqs, featuresleft.mean(axis=0)[c4], label = "left")
-    ax[2].plot(featureExtractor.freqs, featuresright.mean(axis=0)[c4], label = "right")
-    ax[2].set_title("C4")
-    ax[2].grid()
-    ax[2].set_xlim(8,30)
-    ax[2].legend()
-    plt.show()
-
-    with open("testing_left_features.npy", "wb") as f:
-        np.save(f, featuresleft)
--- Desarrollo/PythonScripts/scripts/signals_analysis.py
+++ Desarrollo/PythonScripts/scripts/signals_analysis.py
@@ -14,8 +14,8 @@ eventos = pd.read_csv(eventosFile, sep = ",")
 
 channelsName = ["C3", "CZ", "C4"]
 
-tinit = 1 #el tiempo de inicio se considera ANTES del cue
-tmax = 6 #el tiempo máximo debe considerarse entre el cue y el final del trial
+tinit = -0.1 #el tiempo de inicio se considera ANTES del cue
+tmax = 4.1 #el tiempo máximo debe considerarse entre el cue y el final del trial
 
 trialhandler = TrialsHandler(rawEEG, eventos, tinit = tinit, tmax = tmax, reject=None, sample_rate=250.)
 trialhandler.eventos.head()
@@ -30,10 +30,12 @@ trials_x_clase = np.zeros((len(classesName),int(raw_trials.shape[0]/len(classesN
 for label in classesLabel:
     trials_x_clase[label-1,:,:,:] = raw_trials[labels == label]
 
+trials_x_clase.shape
+
 ### Gráfico de la señal sin filtrar.
 
 trial_n = 1 #Selecciono el trial que quiero clasificar
-label = 2 #Selecciono el label que quiero filtrar
+label = labels[trial_n-1] #Obtengo la etiqueta del trial
 
 eventos.index = eventos["trialNumber"]
 min_tinit = eventos["startingTime"].min()
@@ -88,3 +90,122 @@ plt.suptitle(f"Promedio sobre {trial_n} - Clase {classesName[classesLabel.index(
 #dismiuimos el espacio entre subplots
 plt.tight_layout()
 plt.show()
+
+## Separo por clase
+izq = trials_x_clase[0,:,:,:] #clase 1
+der = trials_x_clase[1,:,:,:] #clase 2
+ambasManos = trials_x_clase[2,:,:,:] #clase 3
+ambosPies = trials_x_clase[3,:,:,:] #clase 4
+rest = trials_x_clase[4,:,:,:] #clase 5
+
+## Extraemos características
+
+from SignalProcessor.FeatureExtractor import FeatureExtractor
+fe_welch = FeatureExtractor(method="welch", sample_rate=250., axisToCompute=3) #instanciamos el extractor de características
+
+trials_x_clase_welch = fe_welch.fit_transform(trials_x_clase)
+
+trial = 3
+indices = np.where((fe_welch.freqs >= 5) & (fe_welch.freqs <= 30))[0]
+
+colors = ["#1e81b0", "#c13f48", "#65cf70", "#f5b041", "#5d6d7e"]
+
+fig, axs = plt.subplots(len(channelsName), 1, figsize=(10, 8))
+for i, channel in enumerate(channelsName):
+    for j, clase in enumerate(classesName):
+        axs[i].plot(fe_welch.freqs[indices], trials_x_clase_welch[j,trial - 1, i, :][indices], label=clase, linewidth=2, color = colors[j])
+        #creamos un rectángulo para resaltar la banda mu
+    axs[i].axvspan(8, 12, alpha=0.1, color='grey')
+    axs[i].set_title(channel)
+    axs[i].set_ylabel('Potencia (dB)')
+    axs[i].set_xlabel('Frecuencia (Hz)')
+    axs[i].legend()
+
+#reduzco el padding entre subplots
+plt.suptitle(f'EEG en la banda $mu$ para trial {trial} - Todas las clases')
+plt.tight_layout()
+plt.show()
+
+
+#Extractor de características para datos de la forma [n_trials, n_channels, n_samples]
+fe_welch = FeatureExtractor(method="welch", sample_rate=250., axisToCompute=2)
+
+#extraemos las características para cada trial
+izq_features = fe_welch.fit_transform(izq)
+der_features = fe_welch.fit_transform(der)
+ambasManos_features = fe_welch.fit_transform(ambasManos)
+ambosPies_features = fe_welch.fit_transform(ambosPies)
+rest_features = fe_welch.fit_transform(rest)
+
+## Graficamos las características para dos clases
+## Obtenemos las frecuencias entre 5 y 30 Hz a partir de fe.self.freqs
+indices = np.where((fe_welch.freqs >= 5) & (fe_welch.freqs <= 30))[0]
+#-1 left, 1 right
+trial = 1
+clase1 = 1
+clase2 = 2
+
+fig, axs = plt.subplots(len(channelsName), 1, figsize=(10, 8))
+for i, channel in enumerate(channelsName):
+    axs[i].plot(fe_welch.freqs[indices], trials_x_clase_welch[clase1-1, trial - 1, i, :][indices], linewidth=2, color = "#1e81b0")
+    axs[i].plot(fe_welch.freqs[indices], trials_x_clase_welch[clase2-1, trial - 1, i, :][indices], linewidth=2, color = "#c13f48")
+    #creamos un rectángulo para resaltar la banda mu
+    axs[i].axvspan(8, 12, alpha=0.1, color='grey')
+    axs[i].set_title(channel)
+    axs[i].set_ylabel('Potencia (dB)')
+    axs[i].set_xlabel('Frecuencia (Hz)')
+    axs[i].legend([classesName[clase1-1], classesName[clase2-1]])
+
+#reduzco el padding entre subplots
+plt.suptitle(f'EEG en la banda $mu$ para trial {trial} - Clases {classesName[clase1-1]} y {classesName[clase1-2]}')
+plt.tight_layout()
+plt.show()
+
+
+## Aplicamos CSP
+##importamos la clase CSP
+from SignalProcessor.CSPMulticlass import CSPMulticlass
+
+cspmulticlass = CSPMulticlass(n_components=2, method = "ovo", n_classes = len(np.unique(labels)), reg = 0.01)
+
+## Separamos en train y test
+from sklearn.model_selection import train_test_split
+#separamos en train y test. Balanceamos las clases
+X_train, X_test, y_train, y_test = train_test_split(raw_trials, labels, test_size=0.2, random_state=42, stratify=labels)
+
+#entrenamos el CSP
+cspmulticlass.fit(X_train, y_train)
+
+##Aplicamos el CSP a los datos de testeo
+X_test_csp = cspmulticlass.transform(X_test)
+X_test_csp.shape
+
+## Extraemos las características de los datos de testeo con CSP
+X_test_csp_wech = fe_welch.fit_transform(X_test_csp)
+
+from SignalProcessor.RavelTransformer import RavelTransformer
+raveltransformer = RavelTransformer() #instanciamos el raveltransformer
+
+X_test_csp_wech_ravel = raveltransformer.fit_transform(X_test_csp_wech)
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+lda = LDA() #instanciamos el clasificador LDA
+
+X_train_csp = cspmulticlass.transform(X_train) #aplico el filtro CSP a los datos de entrenamiento
+X_train_welch = fe_welch.fit_transform(X_train_csp) #aplico el extractor de características welch a los datos de entrenamiento
+trials, componentes, frecuencias = X_train_welch.shape
+
+X_train_welch = X_train_welch[:,:,[indices]].reshape(trials, componentes, len(indices))
+X_train_welch_ravel = raveltransformer.fit_transform(X_train_welch)
+
+lda.fit(X_train_welch_ravel, y_train) #entrenamos el clasificador
+
+X_test_csp = cspmulticlass.transform(X_test) #aplico el filtro CSP a los datos de entrenamiento
+X_test_welch = fe_welch.fit_transform(X_test_csp) #aplico el extractor de características welch a los datos de entrenamiento
+trials, componentes, frecuencias = X_test_welch.shape
+
+X_test_welch = X_test_welch[:,:,[indices]].reshape(trials, componentes, len(indices))
+X_test_welch_ravel = raveltransformer.fit_transform(X_test_welch)
+
+#Evaluamos rápidamente el clasificador sobre los datos de test
+print(f"El accuracy es {lda.score(X_test_welch_ravel, y_test)*100}%")
