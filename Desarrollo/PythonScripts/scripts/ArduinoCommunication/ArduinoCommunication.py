c9943c93f437f4484f849a6e7bcb8f726f9b3020
lucasbaldezzari <lmbaldezzari@gmail.com>
2023-11-06 17:21:36 -0300

    Se realizan pruebas con ArduinoCommunication.py en base a lo solicitado en #30. Se prueba ennviar y recibir datos hacia y desde la placa, respectivamente. TODO OK

--- Desarrollo/PythonScripts/scripts/ArduinoCommunication/ArduinoCommunication.py
+++ Desarrollo/PythonScripts/scripts/ArduinoCommunication/ArduinoCommunication.py
@@ -16,7 +16,7 @@ class ArduinoCommunication:
         Constructor del objeto ArduinoCommunication
     """
 
-    def __init__(self, port, baudrate = 19200, commands = [b'1',b'2',b'3',b'4',b'5'], sleep = 1):
+    def __init__(self, port, baudrate = 19200, commands = [b'1',b'2',b'3',b'4',b'5']):
         """
         Constructor del objeto ArduinoCommunication
 
@@ -38,15 +38,12 @@ class ArduinoCommunication:
         self.baudrate = baudrate
         self.dev = serial.Serial(port, baudrate = baudrate) # Abrimos puerto serie con baudrate de 19200
 
-        ##chequeamos que la lista de comandos no esté vacía con assert
+        #chequeamos que la lista de comandos no esté vacía con assert
         assert commands != [], "La lista de comandos no puede estar vacía"
         self.commands = commands #lista de comandos
 
         self.sessionStatus = None #variable para guardar el estado de la sesión
         self._estadoRobot = None #variable para guardar el estado del robot/actuador
-
-        self.sleep = sleep #tiempo de espera para establecer comunicación serie
-        time.sleep(self.sleep*1000) #esperamos para establecer comunicación serie
         
     def query(self, byte):
         """Enviamos un byte a arduino y recibimos un byte desde arduino
@@ -77,14 +74,9 @@ class ArduinoCommunication:
         
         self.sessionStatus = b"1" #sesión en marcha
         self.moveOrder = b"0" #EL robot empieza en STOP
-        
         self.systemControl = [self.sessionStatus, self.moveOrder]
-        
         self._estadoRobot = self.sendMessage(self.systemControl)
         print("Estado del ROBOT:", self._estadoRobot)
-        
-         #### Actualizamos archivo de estados #####
-        self.estados = [str(self.systemControl[0])[2],str(self.systemControl[1])[2],self._estadoRobot]
         
     def endSesion(self):
         """Se finaliza sesión.
@@ -94,11 +86,8 @@ class ArduinoCommunication:
         
         self.sessionStatus = b"0" #sesión finalizada
         self.moveOrder = b"0" #enviamos un STOP
-        self.systemControl = [self.sessionStatus,
-                             self.moveOrder]
-        
+        self.systemControl = [self.sessionStatus, self.moveOrder]
         self._estadoRobot = self.sendMessage(self.systemControl)
-
         self.close() #cerramos comunicación serie
 
     def getRobotStatus(self):
@@ -110,11 +99,11 @@ class ArduinoCommunication:
         self.dev.close()
 
 if __name__ == "__main__":
-    ard = ArduinoCommunication('COM8')
+    ard = ArduinoCommunication('COM7')
     ard.iniSesion()
     ##enviamos un mensaje de prueba
     sessionStatus = b"1" #sesión en marcha
-    moveOrder = b"1" #Comando 1
+    moveOrder = b"3" #Comando 1
     ard.sendMessage([sessionStatus,moveOrder])
     estadoRobot = ard.getRobotStatus()
     print("Estado del ROBOT:", estadoRobot)
